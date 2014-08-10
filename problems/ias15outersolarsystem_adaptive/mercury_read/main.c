74ff1559a8fac683edaeb5af6e78e0cb67aed929
Hanno Rein <hanno@hanno-rein.de>
2014-08-10 13:11:44 -0400

    am

--- problems/ias15outersolarsystem_adaptive/mercury_read/main.c
+++ problems/ias15outersolarsystem_adaptive/mercury_read/main.c
@@ -9,6 +9,7 @@
 const double k	 	= 0.01720209895;	// Gaussian constant 
 
 double energy();
+double* angular_momentum();
 double energy_init;
 double G;
 long N=7;
@@ -64,6 +65,8 @@ int main (int argc, char* argv[]){
 	readin(argv[1]);
 	int N_1 = N;
 	energy_init = energy();
+	double* aminit;
+	aminit = angular_momentum();
 	readin(argv[2]);
 	int N_2 = N;
 	if (N_1!=N_2){
@@ -73,7 +76,9 @@ int main (int argc, char* argv[]){
 		
 
 	double rel_energy = fabs((energy()-energy_init)/energy_init);
-	printf("%e\n",rel_energy);
+	double* am;
+	am = angular_momentum();
+	printf("%e\t%e\t%e\t%e\t%e\t%e\t%e\n",rel_energy,am[0],am[1],am[2],aminit[0],aminit[1],aminit[2]);
 }
 
 double energy(){
@@ -135,3 +140,83 @@ double energy(){
 }
 
 
+double* angular_momentum(){
+	mpf_t amx;
+	mpf_init(amx);
+	mpf_set_d(amx,0.);
+	mpf_t amy;
+	mpf_init(amy);
+	mpf_set_d(amy,0.);
+	mpf_t amz;
+	mpf_init(amz);
+	mpf_set_d(amz,0.);
+	mpf_t mass;
+	mpf_init(mass);
+	mpf_t vx;
+	mpf_init(vx);
+	mpf_t vy;
+	mpf_init(vy);
+	mpf_t vz;
+	mpf_init(vz);
+	mpf_t x;
+	mpf_init(x);
+	mpf_t y;
+	mpf_init(y);
+	mpf_t z;
+	mpf_init(z);
+	for (int i=0;i<N;i++){
+		mpf_t temp;
+		mpf_init(temp);
+		mpf_set_d(temp,particles[i].vx*particles[i].m);
+		mpf_add(vx, vx, temp);
+		mpf_set_d(temp,particles[i].vy*particles[i].m);
+		mpf_add(vy, vy, temp);
+		mpf_set_d(temp,particles[i].vz*particles[i].m);
+		mpf_add(vz, vz, temp);
+		
+		mpf_set_d(temp,particles[i].x*particles[i].m);
+		mpf_add(x, x, temp);
+		mpf_set_d(temp,particles[i].y*particles[i].m);
+		mpf_add(y, y, temp);
+		mpf_set_d(temp,particles[i].z*particles[i].m);
+		mpf_add(z, z, temp);
+
+
+		mpf_set_d(temp,particles[i].m);
+		mpf_add(mass, mass, temp);
+	}
+	mpf_div(vx,vx,mass);
+	mpf_div(vy,vy,mass);
+	mpf_div(vz,vz,mass);
+	
+	mpf_div(x,x,mass);
+	mpf_div(y,y,mass);
+	mpf_div(z,z,mass);
+
+	for (int i=0;i<N;i++){
+	
+		double dvx = particles[i].vx-mpf_get_d(vx);
+		double dvy = particles[i].vy-mpf_get_d(vy);
+		double dvz = particles[i].vz-mpf_get_d(vz);
+		
+		double dx = particles[i].x-mpf_get_d(x);
+		double dy = particles[i].y-mpf_get_d(y);
+		double dz = particles[i].z-mpf_get_d(z);
+		mpf_t temp;
+		mpf_init(temp);
+		
+		mpf_set_d(temp,particles[i].m * (dy*dvz-dz*dvy));
+		mpf_add(amx, amx,temp);
+		mpf_set_d(temp,particles[i].m * (dz*dvx-dx*dvz));
+		mpf_add(amy, amy,temp);
+		mpf_set_d(temp,particles[i].m * (dx*dvy-dy*dvx));
+		mpf_add(amz, amz,temp);
+		
+	}
+	double* am = malloc(sizeof(double)*3);
+	am[0] = mpf_get_d(amx);
+	am[1] = mpf_get_d(amy);
+	am[2] = mpf_get_d(amz);
+
+	return am;
+}
--- problems/ias15outersolarsystem_adaptive/problem.c
+++ problems/ias15outersolarsystem_adaptive/problem.c
@@ -45,7 +45,9 @@ extern int display_wire;
 #endif // OPENGL
 
 double energy();
+double* angular_momentum();
 double energy_init;
+double* aminit;
 void input_binary_special(char* filename);
 int init_N;
 double timescale = 1;
@@ -76,6 +78,7 @@ void problem_init(int argc, char* argv[]){
 #endif // INTEGRATOR_WH
 	mpf_set_default_prec(512);
 	energy_init = energy();
+	aminit = angular_momentum();
 }
 
 void problem_inloop(){
@@ -152,10 +155,11 @@ void problem_output(){
 void problem_finish(){
 	FILE* of = fopen("energy.txt","w"); 
 	double rel_energy = fabs((energy()-energy_init)/energy_init);
+	double* am = angular_momentum();
 	if (init_N!=N){
 		rel_energy = -1;
 	}
-	fprintf(of,"%e",rel_energy);
+	fprintf(of,"%e\t%e\t%e\t%e\t%e\t%e\t%e\t",rel_energy,am[0],am[1],am[2],aminit[0],aminit[1],aminit[2]);
 	fclose(of);
 }
 
@@ -174,3 +178,83 @@ void input_binary_special(char* filename){
 	fclose(inf);
 }
 
+double* angular_momentum(){
+	mpf_t amx;
+	mpf_init(amx);
+	mpf_set_d(amx,0.);
+	mpf_t amy;
+	mpf_init(amy);
+	mpf_set_d(amy,0.);
+	mpf_t amz;
+	mpf_init(amz);
+	mpf_set_d(amz,0.);
+	mpf_t mass;
+	mpf_init(mass);
+	mpf_t vx;
+	mpf_init(vx);
+	mpf_t vy;
+	mpf_init(vy);
+	mpf_t vz;
+	mpf_init(vz);
+	mpf_t x;
+	mpf_init(x);
+	mpf_t y;
+	mpf_init(y);
+	mpf_t z;
+	mpf_init(z);
+	for (int i=0;i<N;i++){
+		mpf_t temp;
+		mpf_init(temp);
+		mpf_set_d(temp,particles[i].vx*particles[i].m);
+		mpf_add(vx, vx, temp);
+		mpf_set_d(temp,particles[i].vy*particles[i].m);
+		mpf_add(vy, vy, temp);
+		mpf_set_d(temp,particles[i].vz*particles[i].m);
+		mpf_add(vz, vz, temp);
+		
+		mpf_set_d(temp,particles[i].x*particles[i].m);
+		mpf_add(x, x, temp);
+		mpf_set_d(temp,particles[i].y*particles[i].m);
+		mpf_add(y, y, temp);
+		mpf_set_d(temp,particles[i].z*particles[i].m);
+		mpf_add(z, z, temp);
+
+
+		mpf_set_d(temp,particles[i].m);
+		mpf_add(mass, mass, temp);
+	}
+	mpf_div(vx,vx,mass);
+	mpf_div(vy,vy,mass);
+	mpf_div(vz,vz,mass);
+	
+	mpf_div(x,x,mass);
+	mpf_div(y,y,mass);
+	mpf_div(z,z,mass);
+
+	for (int i=0;i<N;i++){
+	
+		double dvx = particles[i].vx-mpf_get_d(vx);
+		double dvy = particles[i].vy-mpf_get_d(vy);
+		double dvz = particles[i].vz-mpf_get_d(vz);
+		
+		double dx = particles[i].x-mpf_get_d(x);
+		double dy = particles[i].y-mpf_get_d(y);
+		double dz = particles[i].z-mpf_get_d(z);
+		mpf_t temp;
+		mpf_init(temp);
+		
+		mpf_set_d(temp,particles[i].m * (dy*dvz-dz*dvy));
+		mpf_add(amx, amx,temp);
+		mpf_set_d(temp,particles[i].m * (dz*dvx-dx*dvz));
+		mpf_add(amy, amy,temp);
+		mpf_set_d(temp,particles[i].m * (dx*dvy-dy*dvx));
+		mpf_add(amz, amz,temp);
+		
+	}
+	double* am = malloc(sizeof(double)*3);
+	am[0] = mpf_get_d(amx);
+	am[1] = mpf_get_d(amy);
+	am[2] = mpf_get_d(amz);
+
+	return am;
+}
